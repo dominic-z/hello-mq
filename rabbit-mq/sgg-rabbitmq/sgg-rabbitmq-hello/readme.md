28d20edfef976bae12f458da62565f7223ef6847
dominiczhu <dominiczhu@tencent.com>
2022-01-18 14:38:46 +0800

    p37

--- rabbit-mq/sgg-rabbitmq/sgg-rabbitmq-hello/readme.md
+++ rabbit-mq/sgg-rabbitmq/sgg-rabbitmq-hello/readme.md
@@ -8,3 +8,25 @@ basicConsume的第三个参数是成功消费信息的回调
 
 重启过docker，之前的队列没持久化，所以hello队列消失了，报错`no queue 'hello' in vhost '/'`
 这里重新声明
+
+# p27
+Worker03并不是没接收到信息，接收到了，只不过接收到之后sleep了；
+
+# p31
+看文档里的吧，比老师讲的好得多了
+mq是预先将消息分配给各个队列的。信道1的qos为2的含义是mq允许这个信道最多有2条未处理的消息；
+
+# p35
+发现了一个神奇的事情，只有关闭了channel和connection，java程序才会关闭结束，否则会一直挂着；
+
+# p37
+这块视频太忽略multiple字段的功能了，异步处理阶段，返回的deliveryTag不是连续的，实际ackConfirmCallback的参数可能是
+(1,false)/(2,false)/(10,true)，
+代表的含义就是，第三个入参的含义是从3~10的消息已经确认，所以multiple是true
+
+可以参考basicAck的注释，如下
+> deliveryTag – the tag from the received AMQP.Basic.GetOk or AMQP.Basic.Deliver
+multiple – true to acknowledge all messages up to and including the supplied delivery tag; false to acknowledge just the supplied delivery tag.
+
+deliveryTag代表的是这次生产或者消费的tag，对于每一个新开启的信道，是从1开始的；
+multiple就看注释吧
--- rabbit-mq/sgg-rabbitmq/sgg-rabbitmq-hello/src/main/java/com/sgg/rabbitmq/utils/RabbitMqUtils.java
+++ rabbit-mq/sgg-rabbitmq/sgg-rabbitmq-hello/src/main/java/com/sgg/rabbitmq/utils/RabbitMqUtils.java
@@ -4,6 +4,9 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * @author dominiczhu
  * @version 1.0
@@ -11,16 +14,31 @@ import com.rabbitmq.client.ConnectionFactory;
  * @date 2022/1/17 11:23 下午
  */
 public class RabbitMqUtils {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws IOException, TimeoutException {
+
+        if (connection == null) {
+            synchronized (RabbitMqUtils.class) {
+                if (connection == null) {
+                    ConnectionFactory factory = new ConnectionFactory();
+                    factory.setHost("127.0.0.1");
+                    factory.setUsername("admin");
+                    factory.setPassword("admin");
+                    factory.setPort(5672);
+                    connection = factory.newConnection();
+                }
+            }
+        }
+        return connection;
+
+    }
+
     //得到一个连接的 channel
     public static Channel getChannel() throws Exception {
         //创建一个连接工厂
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("127.0.0.1");
-        factory.setUsername("admin");
-        factory.setPassword("admin");
-        factory.setPort(5672);
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        Channel channel = getConnection().createChannel();
         return channel;
     }
 }
